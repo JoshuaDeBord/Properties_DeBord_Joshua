5e3f4862b103f24ce40865c592b03d906bfa6925
JoshuaDeBord <114021926+JoshuaDeBord@users.noreply.github.com>
2023-04-25 12:42:32 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Properties_DeBord_Joshua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
